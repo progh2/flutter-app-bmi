2588294c6702c80be39436fb2746e261efc239e0
Gihun Ham <ham@e-mirim.hs.kr)>
2020-08-16 14:27:31 +0900

    #3 결과 화면 ui 작성

--- lib/main.dart
+++ lib/main.dart
@@ -73,9 +73,30 @@ class _BmiMainState extends State<BmiMain> {
 }
 
 class BmiResult extends StatelessWidget {
+  final double height;
+  final double weight;
+
+  BmiResult(this.height, this.weight);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('비만도 계산기')),
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('정상', style: TextStyle(fontSize: 36)),
+            SizedBox(height: 16,),
+            Icon(
+              Icons.sentiment_satisfied,
+              color:Colors.green,
+              size:100,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
