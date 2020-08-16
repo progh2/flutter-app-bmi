af3b36caf696a5b5d537e5c7c48d6ab039c49c16
Gihun Ham <ham@e-mirim.hs.kr)>
2020-08-16 14:19:23 +0900

    #2 입력폼 ui 제작

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,50 @@ class BmiMain extends StatefulWidget {
 }
 
 class _BmiMainState extends State<BmiMain> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('비만도 계산기')),
+      body:Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child:Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '키',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '몸무게'),
+                  keyboardType: TextInputType.number,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top:16.0),
+                    alignment: Alignment.centerRight,
+                  child:RaisedButton(
+                    onPressed: (){
+                      if(_formKey.currentState.validate()){
+                        // 검증시 처리 코드
+                      }
+                    },
+                    child: Text('결과 보기'),
+                  )
+              ),
+            ],
+          )
+        ),
+      ),
+    );
   }
 }
 
