3c1a22e0752a7ffb238efe6948efcce2add128ef
Gihun Ham <ham@e-mirim.hs.kr)>
2020-08-16 13:50:34 +0900

    #1 프로젝트 생성 및 기본코드 작성

---
+++ android/app/src/main/kotlin/kr/hs/emirim/progh2/flutter_app_bmi/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.hs.emirim.progh2.flutter_app_bmi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BmiMain(),
+    );
+  }
+}
+
+class BmiMain extends StatefulWidget {
+  @override
+  _BmiMainState createState() => _BmiMainState();
+}
+
+class _BmiMainState extends State<BmiMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class BmiResult extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
